3b65d4df29464384d0a8542fe2f70db42bbe06f7
Alexandre Kueny <alexandre.kueny@altran.com>
2020-11-20 10:46:30 +0100

    fixed sub process execution

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,38 @@
 #!/usr/bin/env node
 
+import * as program from 'commander';
 
 const chalk = require('chalk');
-const program = require('commander');
 const pkg = require('../package.json');
+const { spawn } = require('child_process');
 
 console.log(chalk.yellow('WELCOME TO GENESE CLI'));
 
-program
-    .version(pkg.version)
+program.version(pkg.version)
     .description(pkg.description);
 
-program
-    .command('cpx [pathToAnalyse]')
+program.command('cpx [pathToAnalyse]')
     .description('Calculates Complexity Index and cyclomatic complexity')
     .option('-l, --language <language>', 'Language: js, ts, jsx, tsx or java', 'ts')
-    .option('-md, --markdown', 'Markdown type report')
-    .option('-c, --console', 'Disable report generation and outputs to console')
-    .option('-r, --refactor', 'EXPERIMENTAL! Enable refactoring report generation')
+    .option('-md, --markdown', 'Markdown type report', false)
+    .option('-c, --console', 'Disable report generation and outputs to console', false)
+    .option('-r, --refactor', 'EXPERIMENTAL! Enable refactoring report generation', false)
     .action((pathToAnalyse, options) => {
-
+        const complexityPath = `${__dirname}/../../../complexity`;
+        const cpxPkg = require(`${complexityPath}/dist/package.json`);
+        spawn('node', [
+            `${complexityPath}/${cpxPkg.bin}`,
+            pathToAnalyse ?? '.',
+            options.language,
+            options.markdown,
+            options.console,
+            options.refactor
+        ], {
+            stdio: ['inherit', 'inherit', 'inherit']
+        });
     });
 
-program
-    .command('api')
+program.command('api')
     .description('Generates Angular or React Api')
     .action(() => {
 
